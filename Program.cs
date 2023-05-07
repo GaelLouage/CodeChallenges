fe1c617db4abf74e59e111bc2a54dfaacb6b2ab3
Gaël Louage <louagegael@hotmail.comgit cleargit config --global user.email louagegael@hotmail.com>
2023-05-07 19:29:46 +0200

    countpositivessumnegatives

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿static int[] CountPositivesSumNegatives(int[] input)
+{
+    return input != null && input.Length != 0
+ ? new int[] { input.Count(x => x > 0), input.Where(x => x < 0).Sum() }
+ : new int[] { };
+}
