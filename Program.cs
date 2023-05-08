f3900f5384848d573cea382e247a3b5486838382
Gaël Louage <louagegael@hotmail.comgit cleargit config --global user.email louagegael@hotmail.com>
2023-05-08 13:10:01 +0200

    sortmystring

--- Program.cs
+++ Program.cs
@@ -4,3 +4,7 @@
  ? new int[] { input.Count(x => x > 0), input.Where(x => x < 0).Sum() }
  : new int[] { };
 }
+
+//Given a string s. You have to return another string such that even-indexed
+//and odd-indexed characters of s are grouped and groups are space-separated (see sample below)
+static string SortMyString(string s) => string.Concat(s.Where((x, i) => i % 2 == 0)) + " " + string.Concat(s.Where((x, i) => i % 2 != 0));
